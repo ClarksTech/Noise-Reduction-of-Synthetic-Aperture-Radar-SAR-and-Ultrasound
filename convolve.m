9d1b5c5c8b53d3cb73eadd68eaba4558d830edaf
ClarksTech <c.rutter50@gmail.com>
2021-11-24 11:39:30 +0000

    convolve added

---
+++ convolve.m
@@ -0,0 +1,19 @@
+function filtered_image = convolve(image_matrix, filter_window)
+%==========================================================================
+% Convolve filter window over image, producing and returning the filtered 
+% image matrix
+%
+% Arguments:
+%   image               image matrix to be convolved over
+%   filter_window       weighted window matrix           
+%
+% Returns:
+%   filtered_image      filtered image matrix
+%
+%==========================================================================
+
+
+
+
+end
+
